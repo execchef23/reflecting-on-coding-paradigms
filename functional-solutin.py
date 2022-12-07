b50308bcb477102c0eedb5495100f9a3a4a8ee0a
Michael Manley <michaelmanley@Michaels-MBP.lan>
2022-12-06 19:33:06 -0500

    OOP function

--- functional-solutin.py
+++ functional-solutin.py
@@ -6,3 +6,18 @@ def flatten_and_sort(array):
         for i in item:
             arr.append(i)
     return sorted(arr)
+
+
+#How does this solution ensure data immutability?
+
+
+#Is this solution a pure function? Why or why not?
+
+
+#Is this solution a higher order function? Why or why not?
+
+
+#Would it have been easier to solve this problem using a different programming style?
+
+
+#Why in particular is functional programming a helpful paradigm when solving this problem?
